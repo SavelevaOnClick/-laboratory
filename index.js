6363bf7987df7610bdeb61f51b84431b586b6956
SavelevaoClick <viktoriyasaveleva3@gmail.com>
2021-10-04 14:38:43 +0300

    add second task

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function test1(strouk1, strouk2) {
+function checkingWhetherItIsAnagram(strouk1, strouk2) {
   if (strouk1.length !== strouk2.length) return "не анаграмма";
   for (let i = 0; i < strouk1.length; i++) {
     if (!test2(strouk1[i], strouk2)) return "не анаграмма";
@@ -6,9 +6,52 @@ function test1(strouk1, strouk2) {
   return "анаграмма";
 }
 
-function test2(letter, strouk2) {
+function checkingLetterForOccurrence(letter, strouk2) {
   for (let i = 0; i < strouk2.length; i++) {
-    if (strouk2[i] === letter) return true;
+    if (strouk2[i].toLowerCase() === letter.toLowerCase()) return true;
   }
   return false;
+}
+
+// second
+
+function secondTask(number) {
+  const stringNumber = String(number);
+  const arrayOfUniqueNumbers = set(number);
+  for (let i = 0; i < arrayOfUniqueNumbers.length; i++) {
+    console.log(
+      `цифра ${
+        arrayOfUniqueNumbers[i]
+      } встречается в числе ${counterOfOccurrencesOfDigitsInAnumber(
+        arrayOfUniqueNumbers[i],
+        stringNumber
+      )}`
+    );
+  }
+}
+
+function counterOfOccurrencesOfDigitsInAnumber(number, stringNumber) {
+  let counter = 0;
+  for (let i = 0; i < stringNumber.length; i++)
+    if (number === stringNumber[i]) {
+      counter++;
+    }
+  return counter;
+}
+function checkingForBeingInAnArray(element, arrayElementsForChecking) {
+  if (arrayElementsForChecking.length === 0) return false;
+  for (let i = 0; i < arrayElementsForChecking.length; i++) {
+    if (element === arrayElementsForChecking[i]) return true;
+  }
+  return false;
+}
+function set(number) {
+  const stringNumber = String(number);
+  const arrayOfUniqueValues = [];
+  for (let i = 0; i < stringNumber.length; i++) {
+    if (!checkingForBeingInAnArray(stringNumber[i], arrayOfUniqueValues)) {
+      arrayOfUniqueValues.push(stringNumber[i]);
+    }
+  }
+  return arrayOfUniqueValues;
 }
