18fec2fd387ae53f0340289047fc931a21da68fa
SavelevaoClick <viktoriyasaveleva3@gmail.com>
2021-10-06 15:48:25 +0300

    add task 12

--- index.js
+++ index.js
@@ -496,3 +496,30 @@ function getMeanOfTheOddNumbersOfMatrix(matrix) {
   return sum / counter;
 }
 // task12
+function getMinToMaxSum(min, max) {
+  let sum = 0;
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+  return sum;
+}
+
+function getMinToMaxSumOfMultiplesOfThree(min, max) {
+  let sum = 0;
+  for (let i = min; i <= max; i++) {
+    if (i % 3 === 0) {
+      sum += i;
+    }
+  }
+  return sum;
+}
+function getMinToMaxSumOfPositiveNumber(min, max) {
+  let sum = 0;
+  for (let i = min; i <= max; i++) {
+    if (i > 0) {
+      sum += i;
+    }
+  }
+  return sum;
+}
+// task14
