8316bfbf87fb5a4036525d01902a61d2823bb289
SavelevaoClick <viktoriyasaveleva3@gmail.com>
2021-10-06 18:09:30 +0300

    add task14

--- index.js
+++ index.js
@@ -523,3 +523,13 @@ function getMinToMaxSumOfPositiveNumber(min, max) {
   return sum;
 }
 // task14
+function getTranspositionMatrix(matrix) {
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < i; j++) {
+      let bufferVariable = matrix[i][j];
+      matrix[i][j] = matrix[j][i];
+      matrix[j][i] = bufferVariable;
+    }
+  }
+  return matrix;
+}
