68686acfa4699dce0ac22960434e25615fa91ac4
SavelevaoClick <viktoriyasaveleva3@gmail.com>
2021-10-06 14:09:30 +0300

    add task11

--- index.js
+++ index.js
@@ -319,3 +319,50 @@ function getDecimal(number) {
   }
   return sum;
 }
+// task11
+function getSumEvenNumbersInMatrix(matrix) {
+  let sum = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] % 2 === 0) {
+        sum += matrix[i][j];
+      }
+    }
+  }
+  return sum;
+}
+
+function getSumMultiplesOfThreeInMatrix(matrix) {
+  let sum = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] % 3 === 0) {
+        sum += matrix[i][j];
+      }
+    }
+  }
+  return sum;
+}
+
+function getSumOddNumbersInMatrix(matrix) {
+  let sum = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] % 2 !== 0) {
+        sum += matrix[i][j];
+      }
+    }
+  }
+  return sum;
+}
+function getPositiveNumbersInMatrix(matrix) {
+  let sum = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] % 2 > 0) {
+        sum += matrix[i][j];
+      }
+    }
+  }
+  return sum;
+}
