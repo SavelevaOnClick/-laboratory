427916fbc02c8bd4f5ffad3231a9f509ef11b569
SavelevaoClick <viktoriyasaveleva3@gmail.com>
2021-10-06 14:33:05 +0300

    add task11 prat2

--- index.js
+++ index.js
@@ -366,4 +366,63 @@ function getPositiveNumbersInMatrix(matrix) {
   }
   return sum;
 }
-// task12
+// task11 part2
+function getAmountElementsZeroInMatrix(matrix) {
+  let counter = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] === 0) {
+        counter++;
+      }
+    }
+  }
+  return counter;
+}
+
+function getAmountElementsNegativeInMatrix(matrix) {
+  let counter = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] < 0) {
+        counter++;
+      }
+    }
+  }
+  return counter;
+}
+
+function getAmountElementsPositiveInMatrix(matrix) {
+  let counter = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] > 0) {
+        counter++;
+      }
+    }
+  }
+  return counter;
+}
+function getAmountElementsPrimeInMatrix(matrix) {
+  function isPrime(number) {
+    if (number < 2 || !number) {
+      return false;
+    }
+    for (let i = 2; i < number; i++) {
+      if (number % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  let counter = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (isPrime(matrix[i][j])) {
+        counter++;
+      }
+    }
+  }
+  return counter;
+}
+// task 12
