0587f7b7137e8352062d5af08a1f5a98b84bdcc5
SavelevaoClick <viktoriyasaveleva3@gmail.com>
2021-10-06 13:21:04 +0300

    add task10

--- index.js
+++ index.js
@@ -296,3 +296,26 @@ function getAmountElementsPrime(array) {
   return counter;
 }
 // task10
+function getBinareNumber(number) {
+  const array = [];
+  do {
+    array.unshift(number % 2);
+    number = Math.floor(number / 2);
+  } while (number !== 1);
+  array.unshift(number);
+
+  return Number(array.join(""));
+}
+
+function getDecimal(number) {
+  let sum = 0;
+  let numberToThePowerOf2 = 0;
+  let counter = 0;
+  const arrayNumber = String(number).split("");
+  for (let i = arrayNumber.length - 1; i >= 0; i--) {
+    numberToThePowerOf2 = arrayNumber[i] * 2 ** counter;
+    sum += numberToThePowerOf2;
+    counter++;
+  }
+  return sum;
+}
