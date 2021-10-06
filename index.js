c94ac6297904e2411163ffc42c8fadb66c804181
SavelevaoClick <viktoriyasaveleva3@gmail.com>
2021-10-06 18:35:06 +0300

    add task 15

--- index.js
+++ index.js
@@ -533,3 +533,15 @@ function getTranspositionMatrix(matrix) {
   }
   return matrix;
 }
+// task15
+function getMatrixAddition(matrix1, matrix2) {
+  const matrixResult = [];
+  for (let i = 0; i < matrix1.length; i++) {
+    matrixResult.push([]);
+    for (let j = 0; j < matrix1[i].length; j++) {
+      let sum = matrix1[i][j] + matrix2[i][j];
+      matrixResult[i][j] = sum;
+    }
+  }
+  return matrixResult;
+}
