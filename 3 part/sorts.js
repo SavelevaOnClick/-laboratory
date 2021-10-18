ce7a0431e9ab6fcd979b0e0a82f8b0c2db15667e
SavelevaoClick <viktoriyasaveleva3@gmail.com>
2021-10-18 12:02:19 +0300

    update

--- 3 part/sorts.js
+++ 3 part/sorts.js
@@ -23,9 +23,9 @@ function bubbleSort(array) {
   for (let i = 0; i < array.length; i++) {
     for (let j = 0; j < array.length; j++) {
       if (array[j] > array[j + 1]) {
-        let a = array[j];
+        let buffer = array[j];
         array[j] = array[j + 1];
-        array[j + 1] = a;
+        array[j + 1] = buffer;
       }
     }
   }
