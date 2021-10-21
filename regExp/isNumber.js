5c83cb6e114e13f5bf64b38d450264c51c84da28
SavelevaoClick <viktoriyasaveleva3@gmail.com>
2021-10-21 18:20:38 +0300

    add regExp

---
+++ regExp/isNumber.js
@@ -0,0 +1,5 @@
+let regExpTelephoneNumber = /\+\d{2}\(\d{3}\)\d{3}\-\d{2}\-\d{2}/;
+let regExpEmail = /\w*\@\w*\.\w{1,5}/;
+let regExpSite = /\http|https\:\/{2}\w*\.\w*/;
+let regExpPassword = /[A-Za-z_0-9]{6,25}/;
+let regExpIpv4 = /([12][1-5][0-5]|[0-9]?)\.([12][1-5][0-5]|[0-9]?)\.([12][1-5][0-5]|[0-9]?)\.([12][1-5][0-5]|[0-9]?)/;
